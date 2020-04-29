f1fa2a3ba2bcabbab1f1ca515332c313535a3eb1
Nat Welch <nat@natwelch.com>
2020-04-29 03:59:10 +0000

    context

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ module.exports = async (app) => {
   createScheduler(app)
   app.on(['check_suite.requested', 'check_run.rerequested'], check)
   app.on('schedule.repository', context => {
+    if (closedRepo(context)) {
+      return
+    }
+
     const license = loadLicense(context)
     const noLicense = (license !== '' || !Object.keys(license).length || license !== 'Other')
     context.log.debug({ license: JSON.stringify(license), noLicense, repo: context.repo() }, 'app got license')
@@ -75,12 +79,18 @@ async function loadLicense (context) {
   }
 }
 
+// TODO: Add error catching
 async function findIssue (context, title) {
-  let id = 0
+  const id = 0
   const opts = context.repo({ state: 'open', per_page: 100 })
   for await (const res of context.github.paginate.iterator(context.github.issues.listForRepo, opts)) {
-    context.log({ res, repo: opts }, "debug issues")
+    context.log({ res, repo: opts }, 'debug issues')
   }
 
   return id
+}
+
+async function closedRepo (context) {
+  const repo = context.github.repos.get(context.repo())
+  return !repo.fork && !repo.archived && repo.has_issues
 }
